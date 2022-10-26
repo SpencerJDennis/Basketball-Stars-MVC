26974a58bf75037dc7b40f322f8d4a7ac7c09d29
tgold83 <ted.jonathan.gold@gmail.com>
2022-10-26 16:01:55 -0700

    add file structure and index views to project
    Co-authored-by: Harold Mesa <harold.mesa@icloud.com>
    Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>

---
+++ BasketballStars/Models/EnvironmentalVariables.cs
@@ -0,0 +1,8 @@
+namespace MvcApiCall.Models
+{
+    public static class EnvironmentVariables
+    {
+        public static string ApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6InVzZXIxIiwibmJmIjoxNjY2NzM5NDAxLCJleHAiOjE2OTgyNzU0MDEsImlhdCI6MTY2NjczOTQwMX0.Y2HTvjipo42L-ji3I-LZk8W2u-mSNZA1uSXRrs5WH8Y";
+    }
+}
+
